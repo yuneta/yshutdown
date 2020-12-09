7c3f3fa6abff0f80ba974bb5f608b17ec46cba0d
Niyamaka <niyamaka@yuneta.io>
2020-12-09 16:13:20 +0100

    yuneta version 4.2.28, all node's api has changed

--- yshutdown.c
+++ yshutdown.c
@@ -22,7 +22,7 @@
 #define NAME        "yshutdown"
 #define DOC         "Shutdown all Yuneta processes, including the agent"
 
-#define APP_VERSION     "4.2.27"
+#define APP_VERSION     "4.2.28"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
