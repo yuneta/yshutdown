fba7501f7113317004925c763af6789bb2351fec
Niyamaka <niyamaka@yuneta.io>
2021-01-21 14:15:13 +0100

    v4.7.1

--- yshutdown.c
+++ yshutdown.c
@@ -22,7 +22,7 @@
 #define NAME        "yshutdown"
 #define DOC         "Shutdown all Yuneta processes, including the agent"
 
-#define APP_VERSION     "4.7.0"
+#define APP_VERSION     "4.7.1"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
