61d660cba60f5b4de134715eef5fd5caa1c1e1b5
Niyamaka <niyamaka@yuneta.io>
2021-11-03 11:18:45 +0100

    v4.22.1 FIX register/find public services in agent

--- yshutdown.c
+++ yshutdown.c
@@ -22,7 +22,7 @@
 #define NAME        "yshutdown"
 #define DOC         "Shutdown all Yuneta processes, including the agent"
 
-#define APP_VERSION     "4.22.0"
+#define APP_VERSION     "4.22.1"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
